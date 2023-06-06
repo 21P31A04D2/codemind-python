a7e74d8ad5f065e74163c304a3b72b0ce1278844
Pavani Yarravula <99954232+21P31A04D2@users.noreply.github.com>
2023-06-06 10:43:05 +0530

    def prime(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True a=int(input()) b=int(input()) i=a d=0 while i<=b:     if prime(i):         d+=1     i+=1 print(d)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,15 +1,17 @@
-from math import sqrt
-def isprime(n):
-    if(n==1):
-        return 0
-    for i in range(2,int(sqrt(n))+1):
-        if(n%i==0):
-            return 0
-    return 1;
-r1=int(input())
-r2=int(input())
-c=0
-for i in range(r1,r2+1):
-    if isprime(i)==1:
-        c+=1
-print(c)        
+def prime(n):
+    if n==1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+a=int(input())
+b=int(input())
+i=a
+d=0
+while i<=b:
+    if prime(i):
+        d+=1
+    i+=1
+print(d)
